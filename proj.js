715e95c4bcefac878ff9b694b1d23d493e62650e
projgrp06 <114412727+projgrp06@users.noreply.github.com>
2022-10-05 21:52:02 -0300

    Update proj.js

--- proj.js
+++ proj.js
@@ -1,3 +1,33 @@
 function carrinho(){
     alert("Item adicionado ao carrinho!");
 }
+
+function validacao(){
+    if (document.getElementById('nome').value == ''){
+        alert('Por favor, informe o seu nome.');
+        document.getElementById('nome').focus();
+        return false;
+    }
+    else if (document.getElementById('nome').value.length < 2){
+        alert('Por favor, informe um nome com no mínimo 2 caracteres.');
+        document.getElementById('nome').focus();
+        return false;
+    }
+
+    if (document.getElementById('email').value == ''){
+        alert('Por favor, informe o seu e-mail.');
+        document.getElementById('email').focus();
+        return false; 
+    }
+
+    if (document.getElementById('mensagem').value == ''){
+        alert('Por favor, preencha o campo com a sua mensagem.');
+        document.getElementById('mensagem').focus();
+        return false; 
+    }
+
+    else {
+        alert('O formulário de contato foi enviado com sucesso.');
+        return true;
+    }
+}
